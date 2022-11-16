d04354f605bc1b44a3f027928ab9011a13b6fbd7
tallner <ctallner@hotmail.com>
2022-11-16 21:22:47 +0100

    Library test class added

---
+++ src/test/java/LibraryTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibraryTests {
+
+    @Test
+    public void searchForOneBookUsingName(){
+
+        String expected = "Book name";
+        String actual = "";
+
+        assertEquals(expected, actual);
+
+    }
+
+
+}
