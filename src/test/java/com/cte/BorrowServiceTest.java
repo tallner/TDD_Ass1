06bfe78b5f6b163ed0fcf615f05fd8b91099b1e2
tallner <ctallner@hotmail.com>
2022-11-30 20:02:28 +0100

    Changed to use @MethodSource syntax

--- src/test/java/com/cte/BorrowServiceTest.java
+++ src/test/java/com/cte/BorrowServiceTest.java
@@ -8,11 +8,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -36,21 +41,36 @@ public class BorrowServiceTest {
         testData = new TestData();
     }
 
+    private static List<Arguments> testInputsAvailableBooks(){
+        return Arrays.asList(
+                Arguments.of("title1",0),
+                Arguments.of("title2",1),
+                Arguments.of("title3",2),
+                Arguments.of("title4",3)
+                );
+    }
+
+    private static List<Arguments> testInputsUnavailableBooks(){
+        return Arrays.asList(
+                Arguments.of("title5",4)
+        );
+    }
+
     @ParameterizedTest
     @DisplayName("Test if book is bookable")
-    @ValueSource(strings = "title1")
-    public void bookingIsPossibleIfBookAvailable(String title) throws IOException {
+    @MethodSource("testInputsAvailableBooks")
+    public void bookingIsPossibleIfBookAvailable(String title, int index) throws IOException {
         BorrowRequest borrowRequest = new BorrowRequest(title);
-        when(searchService.searchTitle(title)).thenReturn(testData.getTestData().get(0));
+        when(searchService.searchTitle(title)).thenReturn(testData.getTestData().get(index));
         assertTrue(borrowService.checkAvailability(borrowRequest));
     }
 
     @ParameterizedTest
     @DisplayName("Book a book and check that is unavailable")
-    @ValueSource(strings = "title1")
-    public void bookingOfOneBookAndCheckItIsBooked(String title) throws IOException {
+    @MethodSource("testInputsAvailableBooks")
+    public void bookingOfOneBookAndCheckItIsBooked(String title, int index) throws IOException {
         BorrowRequest borrowRequest = new BorrowRequest(title);
-        when(searchService.searchTitle(title)).thenReturn(testData.getTestData().get(0));
+        when(searchService.searchTitle(title)).thenReturn(testData.getTestData().get(index));
         String expected = "Book borrowed";
         String actual = borrowService.bookOneBook(borrowRequest);
 
@@ -59,10 +79,10 @@ public class BorrowServiceTest {
 
     @ParameterizedTest
     @DisplayName("Book an unavailable book should not work")
-    @ValueSource(strings = "title5")
-    public void bookingOfAnUnavailableBook(String title) throws IOException {
+    @MethodSource("testInputsUnavailableBooks")
+    public void bookingOfAnUnavailableBook(String title, int index) throws IOException {
         BorrowRequest borrowRequest = new BorrowRequest(title);
-        when(searchService.searchTitle(title)).thenReturn(testData.getTestData().get(4));
+        when(searchService.searchTitle(title)).thenReturn(testData.getTestData().get(index));
 
         String expected = "Book not available";
         String actual = borrowService.bookOneBook(borrowRequest);
@@ -72,15 +92,15 @@ public class BorrowServiceTest {
 
     @ParameterizedTest
     @DisplayName("Book a book and check payment is sent to paymentService")
-    @ValueSource(strings = "title1")
-    public void bookingOfOneBookAndCheckCorrectAmountSentToPayment(String title) throws IOException {
+    @MethodSource("testInputsAvailableBooks")
+    public void bookingOfOneBookAndCheckCorrectAmountSentToPayment(String title, int index) throws IOException {
         BorrowRequest borrowRequest = new BorrowRequest(title);
-        when(searchService.searchTitle(title)).thenReturn(testData.getTestData().get(0));
+        when(searchService.searchTitle(title)).thenReturn(testData.getTestData().get(index));
         borrowService.bookOneBook(borrowRequest);
 
         verify(paymentService, times(1)).pay(payArgumentCaptor.capture());
 
-        int expected = testData.getTestData().get(0).getPrice();
+        int expected = testData.getTestData().get(index).getPrice();
         int actual = payArgumentCaptor.getValue();
 
         assertEquals(expected, actual);
